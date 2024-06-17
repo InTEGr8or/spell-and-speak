0a3ef0ebc06f00a59927852393330297d33ea0f4
InTEGr8or <InTEGr8or@users.noreply.github.com>
2024-06-17 19:53:07 +0000

    deploy: 1f24fc75c4157484ea27b98afe35ddd2c0a34d07

---
+++ precache-manifest.dde7675066007fc19643f5a66f0ec656.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd7a794f1fa12659fce51392ad2a6987",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9fe1a47bffbbcda41aef",
+    "url": "/static/css/main.26bcae03.chunk.css"
+  },
+  {
+    "revision": "307d7ca2c29bbe3e647c",
+    "url": "/static/js/2.64aa7b89.chunk.js"
+  },
+  {
+    "revision": "5e95ff1d1e3d1260b9f9ba117e718af9",
+    "url": "/static/js/2.64aa7b89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39e5cb6a26e3231d630b",
+    "url": "/static/js/3.d57fb3d6.chunk.js"
+  },
+  {
+    "revision": "9fe1a47bffbbcda41aef",
+    "url": "/static/js/main.8f351440.chunk.js"
+  },
+  {
+    "revision": "24aa54375f875c19f65e",
+    "url": "/static/js/runtime-main.e1fd543e.js"
+  }
+]);
